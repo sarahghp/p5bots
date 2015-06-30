7ef5d0d2b89d8d93686ac83db413d44f3b043a05
Sarah GP <sarah.groff.palermo@gmail.com>
2015-06-30 14:13:58 -0400

    Ellipse codes added to quicktests

--- src/server/quicktest.js
+++ src/server/quicktest.js
@@ -46,3 +46,25 @@ p5.board('/dev/cu.usbmodem1421', 'arduino');
 // p.read(["val", "console.log('read val', val)"]);
 // p.read();
 // setInterval(function() { console.log(p.val) }, 500);
+
+// Draw ellipses with a button -- these work on the same 
+// setup as the pin reads
+
+// var p;
+
+// function setup() {
+  
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   p = p5.pin(9, 'DIGITAL', 'INPUT');
+//   p.read();
+
+//   createCanvas(1200, 500);
+//   noStroke();
+// }
+
+// function draw() {
+//   if (p.val) {
+//     fill(Math.random() * 255, Math.random() * 255, Math.random() * 255);
+//     ellipse(Math.random() * width, Math.random() * height, 60, 60);
+//   }
+// }
