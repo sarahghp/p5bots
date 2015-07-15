c8c0a253d459614b7ccaae36fcfbf1a7014d915a
Sarah GP <sarah.groff.palermo@gmail.com>
2015-07-15 13:55:39 -0400

    Tests added to quicktest for LED, RGBLED, Motor

--- src/server/quicktest.js
+++ src/server/quicktest.js
@@ -3,11 +3,32 @@
 /////////////////////////////
 
 // var firmata = require('firmata');
+// var gamma   = require('./gamma.js')
 
 // var board = new firmata.Board('/dev/cu.usbmodem1421', function(err){
 //   if (err) {
 //     throw new Error(err);
 //   }
+
+//   console.log('going');
+
+//   board.pinMode(9, board.MODES.PWM);
+//   board.pinMode(10, board.MODES.PWM);
+//   board.pinMode(11, board.MODES.PWM);
+
+//   board.analogWrite(11, gamma[255]);
+
+//   // setTimeout(function(){
+//   //   board.analogWrite(10, 255);
+//   // }, 1000);
+
+//   // setTimeout(function(){
+//   //   board.analogWrite(11, 255);
+//   // }, 2000);
+  
+//   // board.pinMode(9, board.MODES.PWM);
+//   // board.analogWrite(9, 0);
+
 
 // });
 
@@ -15,7 +36,7 @@
 // Client-Side Sandbox     //
 /////////////////////////////
 
-console.log('quicktest');
+// console.log('quicktest');
 // p5.board('/dev/cu.usbmodem1421', 'arduino');
 
 
@@ -23,27 +44,27 @@ console.log('quicktest');
 // Example Scripts //
 //////////////////////
 
-// Board setup — obviously you may need to change the port
+// // Board setup — obviously you may need to change the port
 // p5.board('/dev/cu.usbmodem1421', 'arduino');
 
 // // Test digital write
 // var p = p5.pin(9, 'DIGITAL', 'OUTPUT');
 // p.write('HIGH');
 
-// // Test PWM write
+// Test PWM write
 // var p = p5.pin(9, 'PWM', 'OUTPUT');
-// p.write(150);
+// p.write(80);
 
 
-// // Test digital read -- only uncomment one p.read() at a time
+// Test digital read -- only uncomment one p.read() at a time
 // var p = p5.pin(9, 'DIGITAL', 'INPUT');
-// p.read(["val", "console.log('read val', val)"]);
+// p.read(function(val){console.log(val);});
 // p.read();
 // setInterval(function() { console.log(p.val) }, 500);
 
 // // Test analog read -- only uncomment one p.read() at a time
 // var p = p5.pin(0, 'ANALOG', 'INPUT');
-// p.read(["val", "console.log('read val', val)"]);
+// p.read(function(val){console.log(val);});
 // p.read();
 // setInterval(function() { console.log(p.val) }, 500);
 
@@ -69,8 +90,8 @@ console.log('quicktest');
 //   }
 // }
 
-// // Click the circle to light the LED -- this works on the same 
-// // setup as the pin writes
+// Click the circle to light the LED -- this works on the same 
+// setup as the pin writes
 // var pin;
 
 // function setup() {
@@ -131,4 +152,151 @@ console.log('quicktest');
 // function draw() {
 //   var val = slider.value();
 //   pin.write(val);
+// }
+
+
+// // LED On & Off -- Same setup as above
+
+// var led;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   led = p5.pin(2, 'LED');
+//   console.log(led);
+// }
+
+// function keyPressed() {
+//   if (keyCode === LEFT_ARROW){
+//     led.on();
+//   } else if (keyCode === RIGHT_ARROW) {
+//     led.off();
+//   } else if (keyCode === UP_ARROW){
+//     led.blink();
+//   } else if (keyCode === DOWN_ARROW) {
+//     led.noBlink();
+//   }
+// }
+
+// // LED Fade on Key -- Same setup as above
+
+// var led;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   led = p5.pin(9, 'LED');
+// }
+
+// function keyPressed(){
+//  if (keyCode === DOWN_ARROW) {
+//   led.write(200);
+//   led.fade(200, 0);
+//  } 
+// }
+
+// // LED Fade in Setup -- Same setup as above [tests queueing]
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   var led = p5.pin(9, 'LED');
+//   led.write(200);
+//   led.fade(200, 0);
+// }
+// 
+
+
+// // RGB LED Write -- Breadboard with common-cathode RGB LED
+// plugged into pins 9, 10, 11
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   var rgb = p5.pin({r: 9, g: 10, b: 11}, 'RGBLED');
+//   var c = color(65);
+//   rgb.write(c);
+//   fill(c);
+//   noStroke();
+//   ellipse(80, 80, 40, 40);
+// }
+
+// RGB LED Read -- Same as above
+
+// var rgb, c;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   rgb = p5.pin({r: 9, g: 10, b: 11}, 'RGBLED');
+//   c = color(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+//   rgb.write(c);
+//   fill(c);
+//   noStroke();
+//   ellipse(80, 80, 40, 40);
+// }
+
+// function keyPressed(){
+//   rgb.read(function(val){ console.log(val.toString()); });
+// }
+
+// // RGB LED On/Off/Blink -- Same as above
+// var rgb, c;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   rgb = p5.pin({r: 9, g: 10, b: 11}, 'RGBLED');
+//   c = color(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+//   rgb.write(c);
+//   fill(c);
+//   noStroke();
+//   ellipse(80, 80, 40, 40); 
+// }
+
+// function keyPressed() {
+//   if (keyCode === LEFT_ARROW){
+//     rgb.on();
+//   } else if (keyCode === RIGHT_ARROW) {
+//     rgb.off();
+//   } else if (keyCode === UP_ARROW){
+//     rgb.blink();
+//   } else if (keyCode === DOWN_ARROW) {
+//     rgb.noBlink();
+//   }
+// }
+
+// RGB LED Fade on Key -- Same setup as above
+
+// var rgb;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   rgb = p5.pin({r: 9, g: 10, b: 11}, 'RGBLED');
+//   var c = color(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+//   rgb.write(c);
+//   fill(c);
+//   noStroke();
+//   ellipse(80, 80, 40, 40); 
+// }
+
+// function keyPressed(){
+//  if (keyCode === DOWN_ARROW) {
+//   rgb.write([200, 200, 200]);
+//   rgb.fade([200, 0, 3000], [200, 0, 5000, 500], [200, 0, 1000, 50]);
+//  } 
+// }
+
+// // Motor tests -- Breadboard with motor power keyed to pin 9
+
+// var motor;
+
+// function setup() {
+//   p5.board('/dev/cu.usbmodem1421', 'arduino');
+//   motor = p5.pin(9, 'MOTOR');
+// }
+
+// function keyPressed() {
+//   if (keyCode === LEFT_ARROW){
+//     motor.on();
+//   } else if (keyCode === RIGHT_ARROW) {
+//     console.log('motor off called');
+//     motor.off();
+//   } else if (keyCode === UP_ARROW){
+//     motor.write(100);
+//   }
 // }
